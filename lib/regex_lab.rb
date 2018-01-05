3cecd8e11677e568ac1f087902957bdeb110ee89
PJ Frias <peejfrias@gmail.com>
2018-01-05 23:32:45 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,11 @@
 require 'pry'
+
 def starts_with_a_vowel?(word)
-  binding.pry
+  if word.match(/\A[aeiou]/)
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
