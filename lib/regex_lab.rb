6af27b3ac2dd39667986a2d8d2ec367ae86db335
PJ Frias <peejfrias@gmail.com>
2018-01-06 01:00:57 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -13,7 +13,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  answer = text.scan(/\A\w{5}\z/)
+  answer = text.scan(/\b\w{5}\b/)
   binding.pry
 end
 
