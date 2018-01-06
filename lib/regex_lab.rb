b5eb1b1549787a05fc998a32d23f48d01001fb9c
PJ Frias <peejfrias@gmail.com>
2018-01-06 01:12:32 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,7 +17,8 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/\A[A-Z].*[.!?]/)
+  answer = text.match(/\A[A-Z].*[.!?]/)
+  binding.pry
 end
 
 def valid_phone_number?(phone)
